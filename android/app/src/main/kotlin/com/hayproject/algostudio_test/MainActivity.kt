5e48d9dda1e9a0435ec5b30a4ee81dddb6d35a58
yayanrw <yayanraw@gmail.com>
2023-04-19 06:45:20 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/hayproject/algostudio_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hayproject.algostudio_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/config/constants.dart
@@ -0,0 +1,4 @@
+const String appTitle = 'Customer Royalty';
+const double defaultRadius = 24.0;
+const double defaultPadding = 16.0;
+const String baseUrl = 'https://api.imgflip.com/get_memes';
