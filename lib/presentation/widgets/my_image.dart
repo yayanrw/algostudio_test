1f89b8b3660421ea6d8a61ad44075a4a8b0e67f3
yayanrw <yayanraw@gmail.com>
2023-04-19 10:42:11 +0700

    add myimage

---
+++ lib/presentation/widgets/my_image.dart
@@ -0,0 +1,19 @@
+import 'package:algostudio_test/core/config/constants.dart';
+import 'package:algostudio_test/domain/entities/meme.dart';
+import 'package:flutter/material.dart';
+
+class MyImage extends StatelessWidget {
+  const MyImage({Key? key, required this.meme}) : super(key: key);
+
+  final Meme meme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(defaultPadding/2),
+      child: Center(
+        child: Image.network(meme.url),
+      ),
+    );
+  }
+}
