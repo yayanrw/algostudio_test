af7e40bec79f79cf92436d22e3e044459ca3b743
yayanrw <yayanraw@gmail.com>
2023-04-19 10:03:20 +0700

    load images

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:algostudio_test/core/config/constants.dart';
+import 'package:algostudio_test/core/utils/request_state.dart';
+import 'package:algostudio_test/presentation/providers/memes_notifier.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 @RoutePage()
 class HomePage extends StatefulWidget {
@@ -11,7 +15,43 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+        () => Provider.of<MemesNotifier>(context, listen: false).fetchMemes());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text(appTitle)),
+      body: Consumer<MemesNotifier>(
+        builder: (context, data, _) {
+          if (data.requestState == RequestState.loading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (data.requestState == RequestState.error) {
+            return Center(
+              child: Text(data.message),
+            );
+          } else {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: data.memes.length,
+              itemBuilder: (context, index) {
+                final meme = data.memes[index];
+                return Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Image.network(meme.url),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
 }
