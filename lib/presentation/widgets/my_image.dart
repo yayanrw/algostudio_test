8064a180fca55c59dd7cbb47f45e8b2b0a8ca1a0
yayanrw <yayanraw@gmail.com>
2023-04-19 11:02:19 +0700

    add loading builder

--- lib/presentation/widgets/my_image.dart
+++ lib/presentation/widgets/my_image.dart
@@ -12,7 +12,23 @@ class MyImage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(defaultPadding),
       child: Center(
-        child: Image.network(meme.url),
+        child: Image.network(
+          meme.url,
+          loadingBuilder: (BuildContext context, Widget child,
+              ImageChunkEvent? loadingProgress) {
+            if (loadingProgress == null) {
+              return child;
+            }
+            return Center(
+              child: CircularProgressIndicator(
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                    : null,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
