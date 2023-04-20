750ad9a61b1c59cb10030536dd1a67a631c023ce
yayanrw <yayanraw@gmail.com>
2023-04-20 09:41:23 +0700

    using provider

--- lib/presentation/pages/detail_page.dart
+++ lib/presentation/pages/detail_page.dart
@@ -1,25 +1,107 @@
+import 'package:algostudio_test/core/config/constants.dart';
 import 'package:algostudio_test/domain/entities/meme.dart';
-import 'package:algostudio_test/presentation/widgets/my_image.dart';
+import 'package:algostudio_test/presentation/providers/detail_notifier.dart';
+import 'package:algostudio_test/presentation/widgets/my_complex_image.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 @RoutePage()
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({Key? key, required this.meme}) : super(key: key);
 
   final Meme meme;
 
   @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(meme.name),
+        title: Text(widget.meme.name),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [MyImage(meme: meme)],
+        child: Consumer<DetailNotifier>(
+          builder: (context, data, _) {
+            return Column(
+              children: [
+                MyComplexImage(
+                    imgUrl: widget.meme.url, addedText: data.addedText),
+              ],
+            );
+          },
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                padding: const EdgeInsets.all(defaultPadding * 2),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.photo_library),
+                      title: const Text('Add Photo'),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.text_fields),
+                      title: const Text('Add Text'),
+                      onTap: () {
+                        Navigator.pop(context);
+                        _showAddTextDialog(context);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _showAddTextDialog(BuildContext context) {
+    String newText = '';
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Text'),
+          content: TextFormField(
+            onChanged: (value) {
+              newText = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('ADD'),
+              onPressed: () {
+                Provider.of<DetailNotifier>(context, listen: false)
+                    .addText(newText);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
