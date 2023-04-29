a0ca5d5da59a7db7e84bf37529afdb6926a1d5ab
yayanrw <yayanraw@gmail.com>
2023-04-29 12:43:35 +0700

    add share to other

--- lib/presentation/pages/generated_image_page.dart
+++ lib/presentation/pages/generated_image_page.dart
@@ -1,14 +1,30 @@
+import 'dart:io';
 import 'dart:typed_data';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
 
 @RoutePage()
 class GeneratedImagePage extends StatelessWidget {
   const GeneratedImagePage({Key? key, required this.myBytes}) : super(key: key);
 
   final Uint8List myBytes;
+
+  Future<void> shareToOthers(Uint8List imageBytes) async {
+    final tempDir = await getTemporaryDirectory();
+    final tempFile = File('${tempDir.path}/image.jpg');
+    await tempFile.writeAsBytes(imageBytes);
+    await Share.shareFiles(
+      [tempFile.path],
+      text: 'Check out this image!',
+      mimeTypes: ['image/jpeg'],
+      sharePositionOrigin: const Rect.fromLTWH(0, 0, 10, 10),
+    );
+    await tempFile.delete();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +43,7 @@ class GeneratedImagePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   IconButton(
-                    icon: Icon(Icons.save),
+                    icon: const Icon(Icons.save),
                     onPressed: () async {
                       final result = await ImageGallerySaver.saveImage(myBytes);
                       if (result['isSuccess']) {
@@ -43,15 +59,9 @@ class GeneratedImagePage extends StatelessWidget {
                     },
                   ),
                   IconButton(
-                    icon: Icon(Icons.facebook),
+                    icon: const Icon(Icons.share),
                     onPressed: () {
-                      // Add logic for sharing the image on Facebook here
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.share),
-                    onPressed: () {
-                      // Add logic for sharing the image on Twitter here
+                      shareToOthers(myBytes);
                     },
                   ),
                 ],
