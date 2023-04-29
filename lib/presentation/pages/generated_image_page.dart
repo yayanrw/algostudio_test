899fcfee64931e1b787d2dc52a241b40da936bc3
yayanrw <yayanraw@gmail.com>
2023-04-29 12:19:56 +0700

    save to gallery func

--- lib/presentation/pages/generated_image_page.dart
+++ lib/presentation/pages/generated_image_page.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 @RoutePage()
 class GeneratedImagePage extends StatelessWidget {
@@ -19,7 +20,42 @@ class GeneratedImagePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.memory(myBytes)
+              Expanded(
+                child: Image.memory(myBytes),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.save),
+                    onPressed: () async {
+                      final result = await ImageGallerySaver.saveImage(myBytes);
+                      if (result['isSuccess']) {
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Image saved to gallery!'),
+                              duration: Duration(seconds: 1),
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.facebook),
+                    onPressed: () {
+                      // Add logic for sharing the image on Facebook here
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: () {
+                      // Add logic for sharing the image on Twitter here
+                    },
+                  ),
+                ],
+              ),
             ],
           ),
         ),
