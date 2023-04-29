15d10362a580ba87ae1e172c72df2fb76325d15f
yayanrw <yayanraw@gmail.com>
2023-04-29 10:41:48 +0700

    add update offset on drag

--- lib/presentation/providers/detail_notifier.dart
+++ lib/presentation/providers/detail_notifier.dart
@@ -7,8 +7,25 @@ class DetailNotifier extends ChangeNotifier {
 
   List<String> get addedText => _addedText;
 
+  List<Offset> _offsets = [];
+
+  List<Offset> get offsets => _offsets;
+
   void addText(String text) {
     _addedText.add(text);
+    _offsets.add(const Offset(0.0, 0.0));
+    notifyListeners();
+  }
+
+  void resetAddedText() {
+    _addedText.clear();
+  }
+
+  void updateOffset(int index, DragUpdateDetails value) {
+    _offsets[index] = Offset(
+      _offsets[index].dx + value.delta.dx,
+      _offsets[index].dy + value.delta.dy,
+    );
     notifyListeners();
   }
 }
