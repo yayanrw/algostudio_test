05eb8c710517ad56898e45df1544a02001091c0b
yayanrw <yayanraw@gmail.com>
2023-04-29 11:06:34 +0700

    add addedImage

--- lib/presentation/providers/detail_notifier.dart
+++ lib/presentation/providers/detail_notifier.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable()
@@ -7,24 +10,53 @@ class DetailNotifier extends ChangeNotifier {
 
   List<String> get addedText => _addedText;
 
-  List<Offset> _offsets = [];
+  List<Offset> _offsetsText = [];
 
-  List<Offset> get offsets => _offsets;
+  List<Offset> get offsetsText => _offsetsText;
+
+  List<File> _addedImage = [];
+
+  List<File> get addedImage => _addedImage;
+
+  List<Offset> _offsetsImage = [];
+
+  List<Offset> get offsetsImage => _offsetsImage;
 
   void addText(String text) {
     _addedText.add(text);
-    _offsets.add(const Offset(0.0, 0.0));
+    _offsetsText.add(const Offset(0.0, 0.0));
     notifyListeners();
   }
 
-  void resetAddedText() {
-    _addedText.clear();
+  Future<void> pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      _addedImage.add(File(pickedFile.path));
+      _offsetsImage.add(const Offset(0.0, 0.0));
+      notifyListeners();
+    }
+    debugPrint('anjing$_addedImage');
   }
 
-  void updateOffset(int index, DragUpdateDetails value) {
-    _offsets[index] = Offset(
-      _offsets[index].dx + value.delta.dx,
-      _offsets[index].dy + value.delta.dy,
+  void reset() {
+    _addedText.clear();
+    _addedImage.clear();
+  }
+
+  void updateOffsetText(int index, DragUpdateDetails value) {
+    _offsetsText[index] = Offset(
+      _offsetsText[index].dx + value.delta.dx,
+      _offsetsText[index].dy + value.delta.dy,
+    );
+    notifyListeners();
+  }
+
+  void updateOffsetImage(int index, DragUpdateDetails value) {
+    _offsetsImage[index] = Offset(
+      _offsetsImage[index].dx + value.delta.dx,
+      _offsetsImage[index].dy + value.delta.dy,
     );
     notifyListeners();
   }
